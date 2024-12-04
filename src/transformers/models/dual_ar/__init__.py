4b1371e0a99242f0b9e44c0fb0cfa7c582c165ae
None1145 <107336646+None1024Outlook@users.noreply.github.com>
2024-12-04 18:52:50 +0800

    Update __init__.py

--- src/transformers/models/dual_ar/__init__.py
+++ src/transformers/models/dual_ar/__init__.py
@@ -3,31 +3,31 @@ from typing import TYPE_CHECKING
 from ...utils import _LazyModule
 from ...utils.import_utils import define_import_structure
 
-_import_structure = {
-    "llama": [
-        "DualARModelArgs",
-        "BaseModelArgs",
-        "NaiveModelArgs",
-        "BaseTransformer",
-        "NaiveTransformer",
-        "DualARTransformer"
-    ]
-}
+# _import_structure = {
+#     "llama": [
+#         "DualARModelArgs",
+#         "BaseModelArgs",
+#         "NaiveModelArgs",
+#         "BaseTransformer",
+#         "NaiveTransformer",
+#         "DualARTransformer"
+#     ]
+# }
 
-from .llama import BaseModelArgs
-from .llama import NaiveModelArgs
+# from .llama import BaseModelArgs
+# from .llama import NaiveModelArgs
 from .llama import DualARModelArgs
-from .llama import BaseTransformer
-from .llama import NaiveTransformer
+# from .llama import BaseTransformer
+# from .llama import NaiveTransformer
 from .llama import DualARTransformer
-from .llama import *
+# from .llama import *
 
-if TYPE_CHECKING:
-    from .llama import *
-    from .lora import *
-    from .lit_module import *
-else:
-    import sys
+# if TYPE_CHECKING:
+#     from .llama import *
+#     from .lora import *
+#     from .lit_module import *
+# else:
+#     import sys
 
-    _file = globals()["__file__"]
-    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
+#     _file = globals()["__file__"]
+#     sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
